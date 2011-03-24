26f545bb6cecba8da307419c0de7f8a364ae75fc
ben <ben@dreamerslab.com>
2011-03-25 05:14:44 +0800

    syntax tuning

--- example_site/application/controllers/contact.php
+++ example_site/application/controllers/contact.php
@@ -4,25 +4,25 @@ class Contact extends CI_Controller{
 
   private $_data;
 
-	public function __construct()
-	{
-		parent::__construct();
+  public function __construct()
+  {
+    parent::__construct();
     $this->load->helper('form');
     $this->_data['nav_selected'] = 'contact';
-	}
-  
-	public function index()
-	{
-    $this->view->render($this->_data);
-	}
+  }
 
-	public function send()
-	{
+  public function index()
+  {
+    $this->view->render($this->_data);
+  }
+
+  public function send()
+  {
     $this->load->model('Contact_Model');
 
     if($this->Contact_Model->validate() == FALSE){
       $this->view->set('action', 'index')->render($this->_data);
-		}else{
+    }else{
       $ajax = $this->input->post('ajax');
 
       if($this->Contact_Model->send() == TRUE){
@@ -39,16 +39,16 @@ class Contact extends CI_Controller{
           $this->view->set('action', 'error')->render($this->_data);
         }
       }
-		}
-	}
+    }
+  }
 
-// -----------------------------------------------------------------------------
-  
+  // -----------------------------------------------------------------------------
+
   private function _render_json($json){
     header('Content-type: application/json');
     echo json_encode($json);
   }
-  
+
 }
 // End of Contact class
 
